96de5fdb1572cd7446fc725f4eab1337add4a67a
karinas98 <karina98savoie@gmail.com>
2023-05-06 13:37:09 +0100

    Deployment

--- src/consts-data.jsx
+++ src/consts-data.jsx
@@ -1,6 +1,5 @@
-export const DEV_API_URL = "http://127.0.0.1:8000";
-// export const API_URL =
-//   process.env.NODE_ENV === "production" ? PROD_API_URL : DEV_API_URL;
+// export const DEV_API_URL = "http://127.0.0.1:8000";
+export const DEV_API_URL = "https://shopandbuy.herokuapp.com/";
 
 export const REACT_APP_CLOUDINARY_UPLOAD_PRESET = "fdmbjiqd";
 export const REACT_APP_CLOUDINARY_CLOUD_NAME = "dkgopzgka";
